52ae96253422e86b8c2c2a031e94564ea0538676
unknown <uxob03@outlook.com>
2021-12-10 11:37:40 -0500

    completed the reducer within reducer/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,52 @@
 
 export const initialState = {
+    smurf: [
+        {
+          id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+          name:'Poppa Smurf',
+          position:'Village Leader',
+          nickname: 'Pops',
+          description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
+        }
+      ],
+      isLoading: false,
+      error: 'Having an issue loading!'
 }
 
 const reducer = ()=>{
+    const reducer = (state = initialState, action) => {
+        switch(action.type) {
+            case START_FETCH:
+                return {
+                    ...state,
+                    smurf: [],
+                    isLoading: true,
+                    error: ''
+                }
+            case SUCCESSFUL_FETCH:
+                return {
+                    smurf: action.payload,
+                    isLoading: false,
+                    error: ''
+                }
+            case ADD_SMURF:
+                return {
+                    smurf: [
+                        ...state.smurf {
+                            name: action.payload.name,
+                            nickname: action.payload.nickname,
+                            position: action.payload.position,
+                            description: action.payload.position
+                        }
+                    ],
+                    isLoading: false,
+                    error: ''
+                }
+        }
+    }
+
 }
+
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
