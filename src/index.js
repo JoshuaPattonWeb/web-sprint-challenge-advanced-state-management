dc0ce9c6242002cce5cb2e017a03316b0b87afe9
unknown <uxob03@outlook.com>
2021-12-10 19:31:58 -0500

    deleted the .gif in the help file that was causing error on code grade and still receiving a zero. Is not fair to students that we have issues with tests nearly everytime. Countless number of spelling errors as well. Someone please review these!

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ ReactDOM.render(
     </Provider>,
     rootElement
 );
-
+//
 //Task List:
 //1. Add in all necessary components and libary methods.
 //2. Create a store that includes thunk and logger middleware support.
